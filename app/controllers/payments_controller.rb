b68558fd5a8b22e92c856b6f06d62303c9e0f5ba
Diego Araujo <diego_shakan@hotmail.com>
2021-08-08 12:21:51 -0300

    feat(payment/receivable): don't allow see another data

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -59,7 +59,7 @@ class PaymentsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_payment
-      @payment = Payment.find(params[:id])
+      @payment = Payment.where(user: current_user).find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
