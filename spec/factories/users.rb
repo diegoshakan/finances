d67259d2714bfeba2b208f340025c0b3bb863f70
Diego Araujo <diego_shakan@hotmail.com>
2021-08-08 12:57:43 -0300

    test(balance): create test to do balance from payments and receivables

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    email { FFaker::Internet.email }
+    password { 'changeme' }
+    password_confirmation { 'changeme' }
+    first_name { FFaker::NameBR.first_name }
+    last_name { FFaker::NameBR.last_name }
   end
 end
