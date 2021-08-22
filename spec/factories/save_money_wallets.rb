c001bb1b4243c6ed399138c3514fc14ecc7eef88
Diego Araujo <diego_shakan@hotmail.com>
2021-08-21 22:06:05 -0300

    chore(save_money): association with user/wallets

--- spec/factories/save_money_wallets.rb
+++ spec/factories/save_money_wallets.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
   factory :save_money_wallet do
-    title { "MyString" }
-    description { "MyText" }
-    value { "9.99" }
+    title { "Investimento" }
+    description { "Ações" }
+    value { rand(100..2000).to_f }
     date { "2021-08-21" }
-    user { nil }
-    save_money { nil }
   end
 end
