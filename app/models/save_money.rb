3a78175ae79d0923d030677eb4be5dfe57f5da3c
Diego Araujo <diego_shakan@hotmail.com>
2022-01-24 16:28:30 -0300

    chore(app) rearrange index wallets

--- app/models/save_money.rb
+++ app/models/save_money.rb
@@ -1,6 +1,8 @@
 class SaveMoney < ApplicationRecord
   belongs_to :user
 
+  default_scope { order(updated_at: :asc) }
+
   has_many :wallets, class_name: 'SaveMoneyWallet', dependent: :destroy
 
   accepts_nested_attributes_for :wallets, allow_destroy: true
